8b05a3f5e05a7214d319bff9e99a16b807e72477
SuneelaSomineni <sominenisuneela@gmail.com>
2024-02-02 12:52:27 +0530

    second commit

--- notifications-send/src/main/java/com/example/notificationssend/service/ReceiverFromListenrs.java
+++ notifications-send/src/main/java/com/example/notificationssend/service/ReceiverFromListenrs.java
@@ -3,7 +3,9 @@ package com.example.notificationssend.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ReceiverFromListenrs {
 
     private static final Logger logger = LoggerFactory.getLogger(ReceiverFromListenrs.class);
